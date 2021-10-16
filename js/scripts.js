6202448573049b106c50a5024504d8cf58621a46
johnstat101 <johnjorogek@gmail.com>
2021-10-16 14:44:53 +0300

    add formula to return day of the week

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,6 @@ function akanName() {
     let year = parseInt(birthDay.slice(2, 4));
     let day = parseInt(birthDay.slice(5, 7));
     let month = parseInt(birthDay.slice(8,10));
-    console.log(day)
-}
+    let dayOfWeek = (((yearCentury/4)-2*yearCentury-1)+((5*year/4))+((26*(month+1)/10))+day)%7;
+    console.log(dayOfWeek);
+};
