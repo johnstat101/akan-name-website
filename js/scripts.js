959070c1b5e837e008ad249d27739c60348f34c2
johnstat101 <johnjorogek@gmail.com>
2021-10-16 13:35:46 +0300

    add methods to slice birthday into century day month

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 // design user input form
 // requirements: Year of birth, month and day of the month
-let birthDay = document.getElementById("birthDay").value
-let gender = document.getElementById("gender").value
-let yearCentury = birthDay.slice(0,4)
-let year = birthDay.slice(2,4)
-let day = birthDay.slice(5,6)
-let month = birthDay.slice(7,8)
-
-console.log(birthDay)
+function akanName() {
+    let birthDay = document.getElementById("birthDay").value;
+    let gender = document.getElementById("gender").value;
+    let yearCentury = parseInt(birthDay.slice(0, 2));
+    let year = parseInt(birthDay.slice(2, 4));
+    let day = parseInt(birthDay.slice(5, 7));
+    let month = parseInt(birthDay.slice(8,10));
+    console.log(day)
+}
