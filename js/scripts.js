02ab8efb78d50b589b847bac614bc98fc11e955f
johnstat101 <johnjorogek@gmail.com>
2021-10-16 15:21:34 +0300

    add a function to return array based on user input gender

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
 // design user input form
 // requirements: Year of birth, month and day of the month
 function akanName() {
-    let birthDay = document.getElementById("birthDay").value;
+    let dayOfWeek = calDayOfWeek();
+    let akanNames = myArrayGen();
+    console.log(akanNames[dayOfWeek]);
+}
+
+function myArrayGen(){
     let gender = document.getElementById("gender").value;
+    var myArray = [];
+    if (gender === "male") {
+        myArray = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    }
+    else {
+        myArray = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    }
+    return myArray;
+}
+
+
+function calDayOfWeek() {
+    let birthDay = document.getElementById("birthDay").value;
     let yearCentury = parseInt(birthDay.slice(0, 2));
     let year = parseInt(birthDay.slice(2, 4));
     let day = parseInt(birthDay.slice(5, 7));
-    let month = parseInt(birthDay.slice(8,10));
-    let dayOfWeek = (((yearCentury/4)-2*yearCentury-1)+((5*year/4))+((26*(month+1)/10))+day)%7;
-    console.log(dayOfWeek);
-};
+    let month = parseInt(birthDay.slice(8, 10));
+    let dayOfWeek = (((yearCentury / 4) - 2 * yearCentury - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7;
+    return dayOfWeek | 0;
+}
