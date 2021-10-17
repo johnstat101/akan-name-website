333eb298d69db7a3af1c1afadae3205d840dbeaa
johnstat101 <johnjorogek@gmail.com>
2021-10-17 21:57:29 +0300

    add function on script that creates a new element <p> on webpage to display akan name

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
-// design user input form
-// requirements: Year of birth, month and day of the month
+// program to return User Akan Names based on birthdate;
+// requirements: Year of birth, month and day of the month;
 function akanName() {
     let dayOfWeek = calDayOfWeek();
     let akanNames = myArrayGen();
+    
+    let addElement = document.createElement("p");
+    let node = document.createTextNode(akanNames[dayOfWeek]);
+    addElement.appendChild(node);
+    
+    const element = document.getElementById("output");
+    element.appendChild(addElement);
     console.log(akanNames[dayOfWeek]);
 }
 
+// A function to return Akan Names array based on user gender
 function myArrayGen(){
+    // declare arrays with Akan Names based on gender
     let gender = document.getElementById("gender").value;
-    var myArray = [];
+    
     if (gender === "male") {
         myArray = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     }
@@ -18,7 +27,7 @@ function myArrayGen(){
     return myArray;
 }
 
-
+// A function to calculate day of the user's birthday
 function calDayOfWeek() {
     let birthDay = document.getElementById("birthDay").value;
     let yearCentury = parseInt(birthDay.slice(0, 2));
@@ -26,5 +35,6 @@ function calDayOfWeek() {
     let day = parseInt(birthDay.slice(5, 7));
     let month = parseInt(birthDay.slice(8, 10));
     let dayOfWeek = (((yearCentury / 4) - 2 * yearCentury - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7;
+    // return an integer value
     return dayOfWeek | 0;
 }
