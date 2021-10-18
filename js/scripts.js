72a100d9b302dcf4a109ee069ecee7c171359441
johnstat101 <johnjorogek@gmail.com>
2021-10-18 16:57:17 +0300

    publish my app on gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,17 @@ function akanName() {
     // call already defined functions
     let dayOfWeek = calDayOfWeek();
     let akanNames = myArrayGen();
-    // capture user name
+    let dayWeekName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        // capture user name
     let userName = document.getElementById("userName").value;
 
-    // check required fields
-    if (userName!= ""){
+    // check required fields if empty!
+    if (userName != "") {
 
         // create html element to append your output
         let addElement = document.createElement("p");
-        let node = document.createTextNode("Hello [" + userName + "] you were born on Day [" +
-            dayOfWeek + "] of the week, and your AKAN Name is: [" + akanNames[dayOfWeek] + "]");
+        let node = document.createTextNode("Hello [" + userName + "!] you were born on " + dayWeekName[dayOfWeek] +
+            ", and your AKAN Name is ~ " + akanNames[dayOfWeek]);
         addElement.appendChild(node);
         // append to your output to our created html element id = "output"
         const element = document.getElementById("output");
@@ -30,24 +31,26 @@ function myArrayGen() {
     // insert control to return arrays with Akan Names based on gender
     if (gender === "male") {
         myArray = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    }
-    else {
+    } else {
         myArray = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     }
     return myArray;
 }
 
+
 // A function to calculate day of the user's birthday
 function calDayOfWeek() {
     // capture user input for birthday
     let birthDay = document.getElementById("birthDay").value;
+
     // slice user birthday to obtain century, year, day & month
     let yearCentury = parseInt(birthDay.slice(0, 2));
     let year = parseInt(birthDay.slice(2, 4));
-    let day = parseInt(birthDay.slice(5, 7));
-    let month = parseInt(birthDay.slice(8, 10));
+    let month = parseInt(birthDay.slice(5, 7));
+    let day = parseInt(birthDay.slice(8, 10));
+
     // apply formula to obtain day of the week based on user input birthday
-    let dayOfWeek = (((yearCentury / 4) - 2 * yearCentury - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7;
-    // return an integer value
-    return dayOfWeek | 0;
+    let dayOfWeek = parseInt((((yearCentury / 4) - 2 * yearCentury - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7);
+    return Math.abs(dayOfWeek);
+
 }
